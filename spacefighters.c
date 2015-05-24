c6b04c5e2570838171eb62c6373241c67ef3e193
Dallos <maheshsullivan@gmail.com>
2015-05-23 20:49:27 -0400

    fixed mergin issues with spacefighter and player library

--- spacefighters.c
+++ spacefighters.c
@@ -1,25 +1,32 @@
-#pragma once
-
 #include <stdio.h>
+#include "entity_living.h"
+#include "player.h"
+#include "stage.h"
+#include "starship.h"
+#include "boss.h"
+#include "entity.h"
 
-void stage_init()
-{
-
+void stage_init(stage_t *stage){
+  /*setup pos and stuff here*/
+  player_t player;
+  player_init(stage, &player, 0);
+  
+  /*game done*/
 }
 
 void stage_run(){
-	/*setup pos and stuff here*/
+  /*setup pos and stuff here*/
   
-	while(stage_step())
-	  {
-	    
-	  }
-	/*game cleanup*/
+  while(stage_step())
+    {
+      
+    }
+  /*game cleanup*/
 }
 
 
 int stage_step(){
-	/*calculate velocity stuff as well as death of players and such*/
+  /*calculate velocity stuff as well as death of players and such*/
 
-	return 1;
+  return 1;
 }
