89e2962188d0dff98e5486e5693eb060eb13fc0e
Dallos <maheshsullivan@gmail.com>
2015-05-24 04:02:56 -0400

    update functions for player and enemy probably work

--- enemy.c
+++ enemy.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 #include "enemy.h"
 #include "vec2.h"
 
 #define ENEMY_ACCELERATION 1
+#define ENEMY_SPEED 5;
 
 void enemy_init(stage_t *stage,
 		 enemy_t* enemy,
@@ -13,15 +15,36 @@ void enemy_init(stage_t *stage,
   starship_init(stage, &enemy->ship, PLAYER_HEALTH, PLAYER_ARMOR, ENEMY, movement);
 }
 
-void enemy_update(enemy_t *enemy)
+void enemy_update(entity_t *entity,stage_t *stage)
 {
-  entity_t *entity = stage->entities;
-  while(entity!=NULL){
+  enemy_t *enemy = (enemy_t *)entity;
+  entity_t *player_entity = stage->entities;
+  double best_distance = INFINITY;
+  entity_t *closest_entity;
+  while(player_entity!=NULL){
     entity_type_t type = entities->type;
     if(type==PLAYER){
-      vec2_t dir=vec2_unitize(vec2_sub(entity->movement->position,enemy->movement->position));
-      
+
+      //checks speed >0 & <max enemy
+
+      double distance=vec2_mag(vec2_sub(player_entity->movement.position,enemy->movement.position));
+      if(distance<best_distance){
+	closest_entity= player_entity;
+	best_distance=distance;
+      }
     }
-    entity = entity->next;
+    player_entity = player_entity->next;
+  }
+  
+  double current_speed=vec2_mag(enemy->movement.velocity);	
+  if(best_distance<70){
+    enemy->movement.velocity=vec2_scale(vec2_unitize(vec2_sub(closest_entity->movement.position,enemy->movement.position)),0.1);
+    starship_fire(enemy);
+  }else if(current_speed <ENEMY_SPEED){
+    enemy->movement.acceleration=vec2_scale(vec2_unitize(vec2_sub(closest_entity->movement.position,enemy->movement.position)),ENEMY_ACCELERATION);
+  }else{
+    enemy->movement.acceleration=vec2_scale(vec2_negate(enemy->movement.orientation),vec2_mag(enemy->movement.acceleration));
   }    
+  entity_update((entity*)enemy);
 }
+  
--- player.c
+++ player.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
 #include "player.h"
+
+#define PLAYER_SPEED 5;
 
 void player_init(player_t* player,
 		 player_id_t player_id,
@@ -9,7 +15,47 @@ void player_init(player_t* player,
   starship_init(&player->ship, PLAYER_HEALTH, PLAYER_ARMOR, PLAYER, movement);
 }
 
-void player_update()
+void player_update(entity_t *entity,stage_t *stage)
 {
+  player_t *player = (player_t *)entity;
+  int do_integer=play_char();
+  
+  double current_speed=vec2_mag(player->movement.velocity);
+  
+  if(current_speed <PLAYER_SPEED){
+    if(ch=='w'){
+      player->movement.acceleration={0,1};
+    } else if(ch=='a'){
+      player->movement.acceleration={-1,0};
+    } else if(ch=='s'){
+      player->movement.acceleration={0,-1};
+    } else if(ch=='d'){
+      player->movement.acceleration={1,0};
+    }
+  }else{
+    player->movement.acceleration=vec2_scale(vec2_negate(player->movement.orientation),vec2_mag(player->movement.acceleration));
+  }
+  
+  if(ch==' '){
+    starship_fire(player);
+  }
+  
+  
+}
 
+int play_char(){
+  //got to check if window focus
+  struct termios oldt, newt;
+  int ch;
+  
+  tcgetattr( STDIN_FILENO, &oldt);
+  newt=oldt;
+  newt.c_lflag &= ~(ICANON);
+  tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+  ch=getchar();
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+  
+  //check stuff, return vals
+  return ch;
+   
 }
