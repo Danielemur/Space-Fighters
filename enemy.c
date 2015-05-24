63870e2ad617994fefb8a8713f0bec58f7b78d60
Dallos <maheshsullivan@gmail.com>
2015-05-24 00:41:37 -0400

    enemy partial update broken though

--- enemy.c
+++ enemy.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "enemy.h"
+#include "vec2.h"
+
+#define ENEMY_ACCELERATION 1
 
 void enemy_init(stage_t *stage,
 		 enemy_t* enemy,
@@ -8,4 +11,17 @@ void enemy_init(stage_t *stage,
 {
   enemy->enemy_id = enemy_id;
   starship_init(stage, &enemy->ship, PLAYER_HEALTH, PLAYER_ARMOR, ENEMY, movement);
+}
+
+void enemy_update(enemy_t *enemy)
+{
+  entity_t *entity = stage->entities;
+  while(entity!=NULL){
+    entity_type_t type = entities->type;
+    if(type==PLAYER){
+      vec2_t dir=vec2_unitize(vec2_sub(entity->movement->position,enemy->movement->position));
+      
+    }
+    entity = entity->next;
+  }    
 }
